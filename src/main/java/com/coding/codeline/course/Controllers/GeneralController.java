89c8b849f2ac06449a319f6c54facf6f0704e035
Muhammad Daniyal Mehmood <43728164+daniyalmehmood@users.noreply.github.com>
2023-05-02 10:55:58 +0400

    Added sample code for security

--- src/main/java/com/coding/codeline/course/Controllers/GeneralController.java
+++ src/main/java/com/coding/codeline/course/Controllers/GeneralController.java
@@ -2,6 +2,7 @@ package com.coding.codeline.course.Controllers;
 
 import com.coding.codeline.course.Slack.SlackClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class GeneralController {
     SlackClient slackClient;
 
     @GetMapping(value = "test")
+    @PreAuthorize("hasRole('USER')")
     public String test(){
         return "${spring.profiles.active}";
     }
@@ -20,5 +22,11 @@ public class GeneralController {
     @GetMapping(value = "slackMessage")
     public void message(@RequestParam String text){
         slackClient.sendMessage(text);
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminOnly() {
+        return "You are an admin!";
     }
 }
--- src/main/java/com/coding/codeline/course/CourseApplication.java
+++ src/main/java/com/coding/codeline/course/CourseApplication.java
@@ -3,9 +3,13 @@ package com.coding.codeline.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CourseApplication {
 
 	public static void main(String[] args) {
